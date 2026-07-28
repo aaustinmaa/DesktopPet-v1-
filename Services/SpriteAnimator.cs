1508a0bfd4d3022aafef0facbbdd854e99876269
Austin Ma <aaustinmaa@gmail.com>
2026-07-28 12:37:22 -0400

    extend sleep animation cycle

--- Services/SpriteAnimator.cs
+++ Services/SpriteAnimator.cs
@@ -215,14 +215,14 @@ namespace DesktopPet.Services
         {
             _sleepZzzLayer.Visibility = Visibility.Visible;
             StartSleepZAnimation(
-                2, 0, 0.0711111111111111, 0.40, 0.40, 0.44,
+                2, 0, 0.064, 0.36, 0.36, 0.396,
                 0.43, 1, -51, 56.5, 0, 0);
             StartSleepZAnimation(
-                1, 0.20, 0.271111111111111, 0.60, 0.60, 0.64,
+                1, 0.18, 0.244, 0.54, 0.54, 0.576,
                 0.54, 1.23, -22, 16, 29, -40.5);
             StartSleepZAnimation(
-                0, 0.40, 0.471111111111111, 0.80, 0.80,
-                0.888888888888889,
+                0, 0.36, 0.424, 0.72, 0.72,
+                0.8,
                 1, 2.29, 0, 0, 51, -56.5);
         }
 
@@ -240,7 +240,7 @@ namespace DesktopPet.Services
             double endX,
             double endY)
         {
-            var duration = TimeSpan.FromSeconds(9);
+            var duration = TimeSpan.FromSeconds(10);
             var opacity = new DoubleAnimationUsingKeyFrames
             {
                 Duration = duration,
